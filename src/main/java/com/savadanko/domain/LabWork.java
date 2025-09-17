431639cb52fa361f750697042780de76a42c9c04
SaveliyDanko <dankosaveliy.m@gmail.com>
2025-09-17 18:27:10 +0300

    bug fix

--- src/main/java/com/savadanko/domain/LabWork.java
+++ src/main/java/com/savadanko/domain/LabWork.java
@@ -24,7 +24,7 @@ public class LabWork {
     private String name;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "coordinates_id", nullable = false, unique = true)
+    @JoinColumn(name = "coordinates_id", nullable = false)
     private Coordinates coordinates;
 
     @CreationTimestamp
